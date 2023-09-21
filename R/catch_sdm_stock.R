e75710600873070faf27f59bc13c8e5b09463867
Cameron Freshwater <ccfreshwater@gmail.com>
2023-09-21 16:07:06 -0700

    update stock-specific analyses

--- R/catch_sdm_stock.R
+++ R/catch_sdm_stock.R
@@ -11,7 +11,11 @@ library(sdmTMBextra)
 
 
 chin <- readRDS(here::here("data", "clean_catch.RDS")) %>% 
-  rename(agg = agg_name)
+  rename(agg = agg_name) %>% 
+  filter(
+    !is.na(agg),
+    fl > 65
+  )
 
 
 chin %>% 
@@ -21,10 +25,8 @@ chin %>%
 
 # calculate total catch across size bins
 catch_stock1 <- chin %>%
-  filter(!is.na(agg),
-         fl > 65,
-         # remove rare stocks
-         !agg %in% c("ECVI", "WCVI", "WA_OR", "Cali")) %>%
+  filter(# remove rare stocks
+         !agg %in% c("ECVI", "Fraser Year.")) %>%
   group_by(event, agg) %>%
   summarize(catch = n(), .groups = "drop") %>%
   ungroup()
@@ -107,19 +109,19 @@ pred_bathy_grid <- readRDS(
 # generate combinations for month/year
 pred_dat <- expand.grid(
   week = c(18.5, 23, 27.5, 32, 36.5),
-  year_f = unique(catch_size$year_f),
-  size_bin = unique(catch_size$size_bin)
+  year_f = unique(catch_stock$year_f),
+  agg = unique(catch_stock$agg)
 ) %>% 
   filter(year_f == "2021") 
 
 pred_grid_list <- vector(mode = "list", length = nrow(pred_dat))
 for (i in seq_along(pred_grid_list)) {
   pred_grid_list[[i]] <- pred_bathy_grid %>% 
-    filter(X < max(catch_size$xUTM + 1000) & X > min(catch_size$xUTM - 1000),
-           Y < max(catch_size$yUTM + 1000) & Y > min(catch_size$yUTM - 1000)) %>% 
+    filter(X < max(catch_stock$xUTM + 1000) & X > min(catch_stock$xUTM - 1000),
+           Y < max(catch_stock$yUTM + 1000) & Y > min(catch_stock$yUTM - 1000)) %>% 
     mutate(week = pred_dat$week[i],
            year_f = pred_dat$year_f[i],
-           size_bin = pred_dat$size_bin[i])
+           agg = pred_dat$agg[i])
 }
 
 pred_grid <- pred_grid_list %>% 
@@ -127,8 +129,8 @@ pred_grid <- pred_grid_list %>%
   mutate(
     xUTM_ds = X / 1000,
     yUTM_ds = Y / 1000,
-    slack_z = 0,
-    moon_z = 0,
+    # slack_z = 0,
+    # moon_z = 0,
     month = case_when(
       week < 23 ~ 5,
       week >= 23 & week < 27 ~ 6,
@@ -137,11 +139,11 @@ pred_grid <- pred_grid_list %>%
       week >= 35 ~ 9
     ),
     month_f = as.factor(month),
-    week_z = (week - mean(catch_size$week)) / sd(catch_size$week),
-    depth_z = (depth - mean(catch_size$mean_depth)) / 
-      sd(catch_size$mean_depth),
-    slope_z = (slope - mean(catch_size$mean_slope)) / 
-      sd(catch_size$mean_slope)
+    week_z = (week - mean(catch_stock$week)) / sd(catch_stock$week),
+    depth_z = (depth - mean(catch_stock$mean_depth)) / 
+      sd(catch_stock$mean_depth),
+    slope_z = (slope - mean(catch_stock$mean_slope)) / 
+      sd(catch_stock$mean_slope)
   ) 
 
 
@@ -162,27 +164,37 @@ plot_map <- function(dat, column) {
 
 
 # fit MVRW version w/ top four stocks
-fit_full <- sdmTMB(
-  catch ~ 0 + (1 | year_f) + agg + depth_z + slope_z + poly(week_z, 2):agg,
-  offset = "offset",
-  data = catch_stock,
-  mesh = sdm_mesh1,
-  family = sdmTMB::nbinom1(),
-  spatial = "on",
-  # spatial_varying = ~ 0 + size_bin,
-  time = "month",
-  spatiotemporal = "rw",
-  groups = "agg",
-  anisotropy = TRUE,
-  share_range = FALSE,
-  silent = FALSE
-)
+# fit_full <- sdmTMB(
+#   catch ~ 0 + (1 | year_f) + agg + depth_z + slope_z + poly(week_z, 2):agg,
+#   offset = "offset",
+#   data = catch_stock,
+#   mesh = sdm_mesh1,
+#   family = sdmTMB::nbinom1(),
+#   spatial = "on",
+#   # spatial_varying = ~ 0 + size_bin,
+#   time = "month",
+#   spatiotemporal = "rw",
+#   groups = "agg",
+#   anisotropy = TRUE,
+#   share_range = TRUE, 
+#   silent = FALSE
+# )
+# 
+# fit_full2 <- update(fit_full, share_range = FALSE)
+# 
+# saveRDS(fit_full2, here::here("data", "fit_mvrfrw_stock.rds"))
 
-fit_full2 <- update(fit_full, anisotropy = FALSE, share_range = TRUE)
+fit_full <- readRDS(here::here("data", "fit_mvrfrw_stock.rds"))
 
 
-# check most complicated model
-mod_in <- f5
+## MODEL CHECKS ----------------------------------------------------------------
+
+sims_nb1 <- simulate(fit_full, nsim = 100, newdata = catch_stock)
+sims_nb1 %>% 
+  dharma_residuals(fit_full)
+
+
+mod_in <- fit_full
 samp <- sample_mle_mcmc(mod_in, mcmc_iter = 130, mcmc_warmup = 100)
 
 obj <- mod_in$tmb_obj
@@ -209,117 +221,182 @@ DHARMa::testResiduals(dharma_res_out)
 
 
 
-# SPATIAL PREDICTION GRID ------------------------------------------------------
+## FIXED EFFECT PREDS ----------------------------------------------------------
 
-# import 1.8 X 1.8 km grid generated in prep_bathymetry.R in chinTagging repo
-pred_bathy_grid <- readRDS(
-  here::here("data", "pred_bathy_grid_utm.RDS"))
-
-# generate combinations for month/year
-pred_dat <- expand.grid(
-  week = seq(min(catch_stock$week), max(catch_stock$week), length = 5),
-  year_f = unique(catch_stock$year_f)
-) %>% 
-  filter(year_f == "2021") 
-
-pred_grid_list <- vector(mode = "list", length = nrow(pred_dat))
-for (i in seq_along(pred_grid_list)) {
-  pred_grid_list[[i]] <- pred_bathy_grid %>% 
-    filter(X < max(catch_stock$xUTM + 1000) & X > min(catch_stock$xUTM - 1000),
-           Y < max(catch_stock$yUTM + 1000) & Y > min(catch_stock$yUTM - 1000)) %>% 
-    mutate(week = pred_dat$week[i],
-           year_f = pred_dat$year_f[i])
-}
-
-pred_grid <- pred_grid_list %>% 
-  bind_rows() %>% 
-  mutate(
-    xUTM_ds = X / 1000,
-    yUTM_ds = Y / 1000,
-    slack_z = 0,
-    moon_z = 0,
-    month = case_when(
-      week <= 22 ~ 5,
-      week > 22 & week <= 26 ~ 6,
-      week > 26 & week <= 30 ~ 7,
-      week > 30 ~ 8
-    ),
-    month_f = as.factor(month),
-    week_z = (week - mean(catch_stock$week)) / sd(catch_stock$week),
-    depth_z = (depth - mean(catch_stock$mean_depth)) / 
-      sd(catch_stock$mean_depth),
-    slope_z = (slope - mean(catch_stock$mean_slope)) / 
-      sd(catch_stock$mean_slope)
-  ) 
-
-
-# helper function for simple predictive maps
-plot_map <- function(dat, column) {
-  ggplot() +
-    geom_raster(data = dat, aes(X, Y, fill = {{ column }})) +
-    coord_fixed() +
-    ggsidekick::theme_sleek() +
-    theme(
-      axis.text = element_blank(),
-      axis.title = element_blank()
+# NOTE month and year values don't matter since random variables and integrated 
+# out
+pred_foo <- function(x = "week", nd, fit) {
+  p <- predict(fit, newdata = nd, se_fit = FALSE, re_form = NA, 
+               re_form_iid = NA, nsim = 100)
+  nd %>% 
+    mutate(
+      est = apply(p, 1, mean),
+      est_se = apply(p, 1, sd),
+      variable = x
     )
 }
+
+nd_week <- expand.grid(
+  year_f = unique(catch_stock$year_f), 
+  week_z = seq(-2, 2, length = 30),
+  month = unique(catch_stock$month), 
+  slack_z = 0,
+  moon_z = 0,
+  agg = unique(catch_stock$agg),
+  depth_z = 0,
+  slope_z = 0
+) %>% 
+  filter(year_f == "2020", 
+         month == "7")
+
+p_week <- pred_foo(x = "week", nd = nd_week, fit = fit_full)
+
+
+# fixed depth effects
+nd_depth <- expand.grid(
+  year_f = unique(catch_stock$year_f),
+  week_z = 0,
+  month = unique(catch_stock$month),
+  slack_z = 0,
+  moon_z = 0,
+  agg = unique(catch_stock$agg),
+  depth_z = seq(-2, 2, length = 30),
+  slope_z = 0
+) %>% 
+  filter(agg == "PugetSo", year_f == "2020", month == "7")
+
+p_depth <- pred_foo(x = "depth", nd = nd_depth, fit = fit_full)
+
+
+# fixed slope effects
+nd_slope <- nd_depth %>% 
+  mutate(
+    slope_z = seq(-2, 2, length = 30),
+    depth_z = 0
+  )
+p_slope <- pred_foo(x = "slope", nd = nd_slope, fit = fit_full)
+
+
+
+full_p <- list(p_week, p_depth, p_slope) %>% 
+  do.call(rbind, .) %>%
+  group_by(agg, variable) %>% 
+  mutate(
+    week = (week_z * sd(catch_stock$week)) + mean(catch_stock$week),
+    depth = (depth_z * sd(catch_stock$mean_depth)) + mean(catch_stock$mean_depth),
+    slope = (slope_z * sd(catch_stock$mean_slope)) + mean(catch_stock$mean_slope),
+    exp_est = exp(est),
+    max_est = max(exp_est),
+    scale_est = exp_est / max_est,
+    up = (est + (stats::qnorm(0.975) * est_se)),
+    lo = (est + (stats::qnorm(0.025) * est_se)),
+    exp_up = exp(up),
+    exp_lo = exp(lo),
+    scale_up = exp(up) / max_est,
+    scale_lo = exp(lo) / max_est
+  ) 
+
+# plots
+plot_foo <- function(var_in = "week", x_lab = "Week") {
+  var_in2 <- rlang::enquo(var_in)
+  dum <- full_p %>% 
+    filter(variable == !!var_in2)
+  ggplot(
+    dum, 
+    aes(x = dum[[var_in]], y = scale_est, ymin = scale_up, 
+        ymax = scale_lo)
+  ) +
+    geom_line() +
+    labs(x = x_lab) +
+    geom_ribbon(alpha = 0.4) +
+    ggsidekick::theme_sleek() +
+    coord_cartesian(y = c(0.03, 3)) +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    theme(axis.title.y = element_blank())
+}
+
+
+week_plot <- plot_foo(var_in = "week", x_lab = "Week") +
+  facet_wrap(~agg) +
+  theme(axis.title.y = element_text(angle = 90)) +
+  ylab("Scaled Abundance")
+depth_plot <- plot_foo(var_in = "depth", x_lab = "Bottom Depth (m)")
+slope_plot <- plot_foo(var_in = "slope", x_lab = "Bottom Slope (degrees)")
+
+p1 <- cowplot::plot_grid(
+  depth_plot, slope_plot,
+  ncol = 2
+)
+
+png(here::here("figs", "ms_figs_stock", "week_fe.png"), res = 250, units = "in", 
+    height = 5.5, width = 5.5)
+week_plot
+dev.off()
+
+png(here::here("figs", "ms_figs_stock", "other_fes.png"), res = 250, units = "in", 
+    height = 5.5, width = 5.5)
+gridExtra::grid.arrange(
+  gridExtra::arrangeGrob(
+    p1, 
+    left = grid::textGrob("Scaled Abundance", rot = 90))
+)
+dev.off()
 
 
 ## SPATIAL PREDICTIONS ---------------------------------------------------------
 
-p3 <- predict(f3, newdata = pred_grid, se_fit = FALSE, re_form = NULL) %>% 
-  mutate(model = "f3",
-         epsilon_st = NA)
-p5 <- predict(f5, newdata = pred_grid, se_fit = FALSE, re_form = NULL) %>% 
-  mutate(model = "f5")
-pp <- rbind(p3, p5) %>% 
-  mutate(exp_est = exp(est))
+upsilon_pred <- predict(fit_full,
+                        newdata = pred_grid,
+                        return_tmb_report = TRUE)
+
+pp <- predict(fit_full, newdata = pred_grid, se_fit = FALSE, re_form = NULL)
+# for now remove extra rows but check w/ Sean
+pp$upsilon_stc <- as.numeric(upsilon_pred$proj_upsilon_st_A_vec)[1:nrow(pp)]
+
+week_key <- data.frame(
+  week = unique(pred_grid$week)
+) %>% 
+  mutate(
+    date = c("May 1", "June 5", "July 10", "Aug 15", "Sep 10") %>% 
+      as.factor() %>% 
+      fct_reorder(., week)
+  )
 
 
-plot_map(pp, omega_s) +
-  scale_fill_gradient2(name = "Spatial\nRF Effect") +
-  facet_wrap(~model)
-
-plot_map(p5, epsilon_st) +
-  scale_fill_gradient2(name = "Spatiotemporal\nRF Effect") +
-  facet_wrap(~month)
-
-plot_map(pp, exp_est) +
-  facet_grid(model~month) +
-  scale_fill_viridis_c()
-
-month_omega <- pp %>%
-  dplyr::select(-year_f) %>% 
+omega <- pp %>%
+  dplyr::select(-c(agg, week, month)) %>%
   distinct() %>% 
-  pivot_longer(cols = starts_with("zeta_s_year"),
-               names_prefix = "zeta_s_year_f",
-               names_to = "year") %>% 
-  plot_map(., value) +
+  plot_map(., omega_s) +
   scale_fill_gradient2(name = "Spatial\nRF Effect") +
-  facet_wrap(~year) +
+  theme(legend.position = "top")
+
+epsilon <- pp %>%
+  plot_map(., upsilon_stc) +
+  scale_fill_gradient2(name = "Spatial\nRF Effect") +
+  facet_grid(agg~month) +
   theme(legend.position = "top")
 
 full_preds <- pp %>% 
-  group_by(size_bin) %>% 
+  group_by(agg) %>% 
   mutate(scale_est = exp(est) / max(exp(est))) %>%
   left_join(., week_key, by = "week") %>% 
   plot_map(., scale_est) +
   scale_fill_viridis_c(trans = "sqrt", name = "Scaled\nAbundance") +
-  facet_grid(date~size_bin)
+  facet_grid(date~agg)
 
 
-png(here::here("figs", "ms_figs", "month_omega.png"), res = 250, units = "in", 
-    height = 7.5, width = 7.5)
-month_omega
+png(here::here("figs", "ms_figs_stock", "omega.png"), res = 250, units = "in", 
+    height = 4.5, width = 4.5)
+omega
 dev.off()
 
-png(here::here("figs", "ms_figs", "size_omega.png"), res = 250, units = "in", 
+png(here::here("figs", "ms_figs_stock", "epsilon.png"), res = 250, units = "in", 
     height = 7.5, width = 7.5)
-size_omega
+epsilon
 dev.off()
 
-png(here::here("figs", "ms_figs", "spatial_preds.png"), res = 250, units = "in", 
+png(here::here("figs", "ms_figs_stock", "spatial_preds.png"), res = 250, units = "in", 
     height = 7.5, width = 7.5)
 full_preds
 dev.off()
