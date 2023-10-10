9267e0d25877917fdc4a98c66a7bd9b50b7db841
Cameron Freshwater <ccfreshwater@gmail.com>
2023-10-10 14:55:08 -0700

    update study area figs

--- R/maps.R
+++ R/maps.R
@@ -15,84 +15,133 @@ sets <- readRDS(here::here("data", "cleanSetData.RDS")) %>%
   # remove sets not on a troller
   filter(!grepl("rec", event))
 
-chin <- readRDS(here::here("data", "cleanTagData_GSI.RDS")) %>% 
-  mutate(
-    month = lubridate::month(deployment_time),
-    month = case_when(
-      month %in% c(4, 5) ~ 5,
-      month %in% c(8, 9) ~ 8,
-      TRUE ~ month
-    ),
-    month_f = as.factor(month),
-    size_class = case_when(
-      fl < 65 ~ "small",
-      fl >= 65 & fl < 75 ~ "medium",
-      fl >= 75 ~ "large"
-    ),
-    size_class = as.factor(size_class),
-    year = as.factor(year),
-    agg_name = fct_relevel(agg_name, "WCVI", "ECVI", "Fraser", "PugetSo",
-                           "Col", "WA_OR", "Cali")
-  ) %>% 
-  filter(event %in% sets$event)
+# chin <- readRDS(here::here("data", "cleanTagData_GSI.RDS")) %>% 
+#   mutate(
+#     month = lubridate::month(deployment_time),
+#     month = case_when(
+#       month %in% c(4, 5) ~ 5,
+#       month %in% c(8, 9) ~ 8,
+#       TRUE ~ month
+#     ),
+#     month_f = as.factor(month),
+#     size_class = case_when(
+#       fl < 65 ~ "small",
+#       fl >= 65 & fl < 75 ~ "medium",
+#       fl >= 75 ~ "large"
+#     ),
+#     size_class = as.factor(size_class),
+#     year = as.factor(year),
+#     agg_name = fct_relevel(agg_name, "WCVI", "ECVI", "Fraser", "PugetSo",
+#                            "Col", "WA_OR", "Cali")
+#   ) %>% 
+#   filter(event %in% sets$event)
 
 
 ## STUDY AREA ------------------------------------------------------------------
 
-wCan <- map_data("worldHires", region = c("usa", "canada")) %>%
-  filter(long < -110) %>%
-  fortify(.)
+set_dat <- readRDS(here::here("data", "cleanSetData.RDS")) 
+
+# map data
+coast <- rbind(rnaturalearth::ne_states( "United States of America",
+                                         returnclass = "sf"),
+               rnaturalearth::ne_states( "Canada", returnclass = "sf"))
+coast_trim <- sf::st_crop(
+  coast,
+  xmin = -128.5, ymin = 48, xmax = -122.5, ymax = 51.5
+) %>% 
+  sf::st_transform(., crs = sf::st_crs("+proj=utm +zone=10 +units=m"))
+
+bath_grid <- readRDS(here::here("data", "pred_bathy_grid_1000m.RDS")) %>% 
+  mutate(id = row_number(),
+         shore_dist_km = shore_dist / 1000) %>% 
+  filter(X < max(set_dat$xUTM - 500),
+         X > min(set_dat$xUTM + 1000),
+         Y < max(set_dat$yUTM - 500),
+         Y > min(set_dat$yUTM + 500))
+
+crit_hab <- sf::st_read(
+  here::here("data", "critical_habitat_trim", 
+             "Proposed_RKW_CriticalHabitat update_SWVI_CSAS2016_shrunk.shp")) %>% 
+  sf::st_transform(., crs = sf::st_crs("+proj=utm +zone=10 +units=m")) %>% 
+  sf::st_crop(., 
+              xmin = min(set_dat$xUTM + 1000), 
+              ymin = max(set_dat$yUTM - 500), 
+              xmax = max(set_dat$xUTM - 500), 
+              ymax = min(set_dat$yUTM + 500))
 
 
-# plot relative to critical habitat
-coast <- readRDS(here::here("data", "crop_coast_sf.rds")) %>% 
-  st_crop(., xmin = -127, ymin = 48, xmax = -124, ymax = 49.1) 
-crit_hab_full <- raster::shapefile(
-  here::here("data", "critical_habitat", 
-             "Proposed_RKW_CriticalHabitat update_SWVI_CSAS2016.shp")) %>% 
-  sp::spTransform(., CRS("+proj=longlat +datum=WGS84"))
-ggplot() +
-  geom_sf(data = coast, color = "black", fill = "white", size = 1.25) +
-  geom_polygon(data = crit_hab_full, aes(x = long, y = lat, group = group), 
-             colour = "red", fill = NA, lwd = 1) +
-  geom_point(data = sets, aes(x = lon, y = lat, colour = as.factor(year)),
-             inherit.aes = FALSE, alpha = 0.2)
-  
+base_map <- ggplot() + 
+  ggsidekick::theme_sleek() +
+  theme(axis.title = element_blank(),
+        legend.position = "top") +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0))
+main_map <- base_map +
+  geom_sf(data = coast_trim, fill = "darkgrey") +
+  geom_sf(data = crit_hab, colour = "red", fill = "transparent", lty = 2) #+
+  # theme(axis.text = element_blank())
+depth_map <- base_map +
+  geom_raster(data = bath_grid, aes(x = X, y = Y, fill = depth)) +
+  scale_fill_viridis_c(name = "Bottom\nDepth (m)", direction = -1)  +
+  theme(legend.position = "right",
+        axis.text = element_blank())
+slope_map <- base_map +
+  geom_raster(data = bath_grid, aes(x = X, y = Y, fill = slope)) +
+  scale_fill_viridis_c(name = "Bottom\nSlope\n(degrees)", direction = -1)  +
+  theme(legend.position = "right",
+        axis.text = element_blank())
 
-crop_coast <- coast %>% 
-  st_crop(., xmin = -126.3, ymin = 48.45, xmax = -125.15, ymax = 49) 
+crop_coast <- readRDS(here::here("data", "crop_coast_sf.RDS")) %>% 
+# crop_coast <- coast %>%
+  sf::st_crop(., xmin = -126.3, ymin = 48.45, xmax = -125.1, ymax = 49)
 
 sets_only <- ggplot() +
   geom_sf(data = crop_coast, color = "black", fill = "white", size = 1.25) +
-  geom_point(data = sets, aes(x = lon, y = lat, colour = as.factor(year)),
-             inherit.aes = FALSE, alpha = 0.2) +
+  geom_point(data = sets, aes(x = lon, y = lat, fill = as.factor(year)),
+             inherit.aes = FALSE, alpha = 0.4, shape = 21) +
   ggsidekick::theme_sleek() +
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_continuous(expand = c(0, 0)) +
-  theme(axis.title = element_blank(),
-        legend.title = element_blank()) 
-
-stock_catch <- ggplot() +
-  geom_sf(data = crop_coast, color = "black", fill = "white", size = 1.25) +
-  geom_point(data = chin,
-             aes(x = lon, y = lat, fill = size_class),
-             inherit.aes = FALSE, shape = 21, alpha = 0.5) +
-  ggsidekick::theme_sleek() +
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  scale_fill_viridis_d() +
+  scale_fill_discrete(guide = "none") +
   theme(axis.title = element_blank(),
         legend.title = element_blank(),
-        axis.text = element_blank()) +
-  facet_grid(agg_name~month)
+        axis.text = element_blank()) 
 
+p1 <- cowplot::plot_grid(
+  depth_map, slope_map,
+  ncol = 1
+)
+p2 <- cowplot::plot_grid(
+  main_map, sets_only,
+  ncol = 1
+)
+p3 <- cowplot::plot_grid(
+  plotlist = list(p2, p1),
+  ncol = 2,
+  rel_widths = c(1, 1.2)
+)
 
-png(here::here("figs", "ms_figs", "set_map.png"), res = 250, units = "in", 
-    height = 5.5, width = 5.5)
-sets_only
+png(here::here("figs", "ms_figs", "study_area.png"), res = 250, units = "in", 
+    height = 5.5, width = 7.5)
+p3
 dev.off()
 
-png(here::here("figs", "ms_figs", "stock_catch_map.png"), 
-    res = 250, units = "in", height = 5.5, width = 5.5)
-stock_catch
-dev.off()
+# stock_catch <- ggplot() +
+#   geom_sf(data = crop_coast, color = "black", fill = "white", size = 1.25) +
+#   geom_point(data = chin,
+#              aes(x = lon, y = lat, fill = size_class),
+#              inherit.aes = FALSE, shape = 21, alpha = 0.5) +
+#   ggsidekick::theme_sleek() +
+#   scale_x_continuous(expand = c(0, 0)) +
+#   scale_y_continuous(expand = c(0, 0)) +
+#   scale_fill_viridis_d() +
+#   theme(axis.title = element_blank(),
+#         legend.title = element_blank(),
+#         axis.text = element_blank()) +
+#   facet_grid(agg_name~month)
+# 
+# 
+# png(here::here("figs", "ms_figs", "stock_catch_map.png"), 
+#     res = 250, units = "in", height = 5.5, width = 5.5)
+# stock_catch
+# dev.off()
